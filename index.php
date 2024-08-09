a5c2ac569ec9c6fde3e374f5b35b2cbc45f62936
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-08-09 17:56:08 +0500

    commit

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
