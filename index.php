3eae37e20afee593448c613faafec0870ca3bbd4
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-08-09 17:10:26 +0500

    commit

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <meta name="description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998997799333.">
     <meta name="keywords" content="ilhomjonov, iqbolshoh, iqbolshoh ilhomjonov, ikromjonovich, iqbolshoh_777, programmer_777, software developer, full-stack developer, javascript developer, web developer, programmer, samarkand developer, uzbekistan developer, programming, web development, github projects, software engineer, dasturchi, programmist, dasturiy taminot, web dasturchi, iqbolshoh.uz, templates.uz, +998997799333, 997799333">
 
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Iqbolshoh Ilhomjonov">
+
     <meta property="og:title" content="Iqbolshoh Ilhomjonov - Full-Stack Developer">
     <meta property="og:description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998997799333.">
     <meta property="og:image" content="./images/iqbolshoh-1.jpg">
@@ -16,9 +19,6 @@
     <meta property="og:url" content="https://templates.uz">
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Iqbolshoh Ilhomjonov">
-
-    <meta name="robots" content="index, follow">
-    <meta name="author" content="Iqbolshoh Ilhomjonov">
 
     <title>Iqbolshoh Ilhomjonov</title>
     <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
