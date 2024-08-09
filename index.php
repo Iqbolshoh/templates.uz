e31d8e1860f4e71018840855355b6c6963a76353
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-08-09 17:49:11 +0500

    commit

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="Content-Language" content="en">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998997799333.">
+    <meta name="description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998(99)7799333.">
     <meta name="keywords" content="ilhomjonov, iqbolshoh, iqbolshoh ilhomjonov, ikromjonovich, iqbolshoh_777, programmer_777, software developer, full-stack developer, javascript developer, web developer, programmer, samarkand developer, uzbekistan developer, programming, web development, github projects, software engineer, dasturchi, programmist, dasturiy taminot, web dasturchi, iqbolshoh.uz, templates.uz, +998997799333, 997799333">
 
     <meta name="robots" content="index, follow">
     <meta name="author" content="Iqbolshoh Ilhomjonov">
 
     <meta property="og:title" content="Iqbolshoh Ilhomjonov - Full-Stack Developer">
-    <meta property="og:description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998997799333.">
+    <meta property="og:description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998(99)7799333.">
     <meta property="og:image" content="./images/iqbolshoh-1.jpg">
     <meta property="og:image:width" content="1600">
     <meta property="og:image:height" content="900">
