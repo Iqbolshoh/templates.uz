5f1e17a5b96520e8a305b46dd4e4d4d0d841a686
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-09-05 15:24:15 +0500

    commit

--- index.php
+++ index.php
@@ -5,58 +5,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="Content-Language" content="en">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998(99)7799333.">
-    <meta name="keywords" content="ilhomjonov, iqbolshoh, iqbolshoh ilhomjonov, ikromjonovich, iqbolshoh_777, programmer_777, software developer, full-stack developer, javascript developer, web developer, programmer, samarkand developer, uzbekistan developer, programming, web development, github projects, software engineer, dasturchi, programmist, dasturiy taminot, web dasturchi, iqbolshoh.uz, templates.uz, +998997799333, 997799333">
+    <meta name="description"
+        content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998(99)7799333.">
+    <meta name="keywords"
+        content="ilhomjonov, iqbolshoh, iqbolshoh ilhomjonov, ikromjonovich, iqbolshoh_777, programmer_777, software developer, full-stack developer, javascript developer, web developer, programmer, samarkand developer, uzbekistan developer, programming, web development, github projects, software engineer, dasturchi, programmist, dasturiy taminot, web dasturchi, iqbolshoh.uz, templates.uz, +998997799333, 997799333">
 
     <meta name="robots" content="index, follow">
     <meta name="author" content="Iqbolshoh Ilhomjonov">
 
     <meta property="og:title" content="Iqbolshoh Ilhomjonov - Full-Stack Developer">
-    <meta property="og:description" content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998(99)7799333.">
-    <meta property="og:image" content="./images/iqbolshoh-1.jpg">
-    <meta property="og:image:width" content="1600">
-    <meta property="og:image:height" content="900">
+    <meta property="og:description"
+        content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998(99)7799333.">
     <meta property="og:url" content="https://templates.uz">
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Iqbolshoh Ilhomjonov">
 
     <title>Iqbolshoh Ilhomjonov</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
     <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
-
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        background-color: #f0f0f0;
-        padding: 20px;
-    }
-
-    .contact-info {
-        background-color: #ffffff;
-        border-radius: 10px;
-        padding: 20px;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        max-width: 300px;
-        margin: 0 auto;
-    }
-
-    .contact-info h2 {
-        margin-top: 0;
-    }
-
-    .contact-info a {
-        color: #007bff;
-        text-decoration: none;
-    }
-
-    .contact-info a:hover {
-        text-decoration: underline;
-    }
-</style>
 
 
 <body>
