017224665689ea753585a16440cb562e75f11c59
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-09 11:44:41 +0500

    commit

--- admin/features.php
+++ admin/features.php
@@ -28,7 +28,6 @@ if (isset($_GET['edit'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Features</title>
     <link href="../favicon.ico" rel="icon">
     <?php include 'includes/css.php'; ?>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
