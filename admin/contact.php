a0c4128e688ee8e008d591dc9029bf06e9303bc3
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-09 11:44:30 +0500

    commit

--- admin/contact.php
+++ admin/contact.php
@@ -77,7 +77,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <title>Contact</title>
     <link href="../favicon.ico" rel="icon">
     <!-- CSS -->
     <?php include 'includes/css.php'; ?>
