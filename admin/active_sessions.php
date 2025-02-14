1f8388374a7bda25dd2f9544f2565b8ce5679c59
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-14 09:46:13 +0500

    commit

--- admin/active_sessions.php
+++ admin/active_sessions.php
@@ -36,7 +36,7 @@ if (isset($_POST['update_session'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="stylesheet" href="../src/css/adminlte.min.css">
+    <link rel="stylesheet" href="../assets/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -139,9 +139,9 @@ if (isset($_POST['update_session'])) {
         }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="../src/js/jquery.min.js"></script>
-    <script src="../src/js/bootstrap.bundle.min.js"></script>
-    <script src="../src/js/adminlte.min.js"></script>
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/adminlte.min.js"></script>
 </body>
 
 </html>
