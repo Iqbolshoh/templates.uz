776bcf01115948fb5c45158daf4156dddd212b2a
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-14 09:42:05 +0500

    commit

--- admin/index.php
+++ admin/index.php
@@ -53,7 +53,7 @@ if (
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="stylesheet" href="../src/css/adminlte.min.css">
+    <link rel="stylesheet" href="../assets/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
