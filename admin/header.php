8d49af20fd11f5ef727ea629f2d4e0c423cf81bf
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-14 09:45:05 +0500

    commit

--- admin/header.php
+++ admin/header.php
@@ -128,12 +128,12 @@ $activePage = $activePageInfo['activePage'] ?? null;
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="./" class="brand-link">
-        <img src="../src/images/logo.png" alt="Admin Panel Logo" class="brand-image img-circle elevation-3">
+        <img src="../assets/img/logo.png" alt="Admin Panel Logo" class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">Admin Panel</span>
     </a>
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image"><img src="../src/images/default.png" class="img-circle elevation-2" alt="User Image">
+            <div class="image"><img src="../assets/img/default.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info"><a href="./" class="d-block">Iqbolshoh Ilhomjonov</a></div>
         </div>
