ecce0fc0301331017368ca4b5d516852a8d64290
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-14 11:01:33 +0500

    commit

--- admin/includes/js.php
+++
@@ -1,4 +0,0 @@
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../assets/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/sweetalert2.min.js"></script>
-<script src="../assets/js/adminlte.min.js"></script>
